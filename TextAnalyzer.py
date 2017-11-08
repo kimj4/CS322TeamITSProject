b91cdd2cbc542b348676da233be46ddb479e944a
posegae <posegae@carleton.edu>
2017-11-07 21:53:02 -0600

    text analyzer now theoretically calculates unigram and bigram MLE for upspeak model

--- TextAnalyzer.py
+++ TextAnalyzer.py
@@ -1,29 +1,45 @@
 import EmailProcessor
 import json
 
-def calculateMLE(sentence, model):
+def calculateMLE(N, sentence):
     '''
     calculates the minimum likelihood estimate on sample sentences for given model
     '''
 
+    with open('models/upspeakUnigramModel.json', 'r') as fp:
+        upspeakUnigramModel = json.load(fp)
+    with open('models/downspeakUnigramModel.json', 'r') as fp:
+        downspeakUnigramModel = json.load(fp)
+    with open('models/upspeakBigramModel.json', 'r') as fp:
+        upspeakBigramModel = json.load(fp)
+    with open('models/downspeakBigramModel.json', 'r') as fp:
+        downspeakBigramModel = json.load(fp)
+
+
+    sentenceNGrams = EmailProcessor.createNgram(N, [sentence])
     sentenceMLE = 1;
-    testNGrams = createNgram(N, [sentence])
-    for key, value in testNGrams.items():
-        # if N > 1:
-        #     if key.split(' ')[-2] == '<s>':
-        #         sentenceMLE = sentenceMLE * (oneGram['<s>'] / oneGramTotal) * testNGrams[key]
-        #     elif key in probabilityDict.keys():
-        #         # print('n-gram found')
-        #         sentenceMLE = sentenceMLE * probabilityDict[key] * testNGrams[key]
-        #     else:
-        #         sentenceMLE = sentenceMLE * 0.000001
-        # else:
-        if key in probabilityDict.keys():
-            # print('n-gram found')
-            # print(str(probabilityDict[key]))
-            sentenceMLE = sentenceMLE * probabilityDict[key] * testNGrams[key]
+    totalUpspeakUnigrams = sum(upspeakUnigramModel.values())
+
+    # right now, only calculates upspeak MLE, specific to bigrams/unigrams
+    for key, value in sentenceNGrams.items():
+        if N > 1:
+            if key.split(' ')[-2] == '<s>':
+                sentenceMLE = sentenceMLE * (upspeakUnigramModel['<s>'] / totalUpspeakUnigrams) * sentenceNGrams[key]
+            elif key in upspeakBigramModel.keys():
+                # print('n-gram found')
+                # calculating P(w1 w2 | w1)
+                prevGram = " ".join(key.split(" ")[:-1])
+                sentenceMLE = sentenceMLE * (upspeakBigramModel[key] / upspeakUnigramModel[prevGram]) * sentenceNGrams[key]
+            else:
+                sentenceMLE = sentenceMLE * 0.000001
         else:
-            sentenceMLE = sentenceMLE * 0.000001
+            if key in upspeakBigramModel.keys():
+                # print('n-gram found')
+                # print(str(probabilityDict[key]))
+                sentenceMLE = sentenceMLE * (upspeakUnigramModel[key] / totalUpspeakUnigrams) * sentenceNGrams[key]
+            else:
+                sentenceMLE = sentenceMLE * 0.000001
+
 
     return sentenceMLE
 
@@ -33,6 +49,7 @@ def main():
         upspeakUnigramModel = json.load(fp)
     print(upspeakUnigramModel)
     print(type(upspeakUnigramModel))
+    calculateMLE(2, 'Please get in touch soon')
 
 if __name__ == '__main__':
     main()
