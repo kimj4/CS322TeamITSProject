95bb4ded76670f20942e42b81c8fdd9b34945332
Ju Yun Kim <kimj4@carleton.edu>
2017-11-19 17:25:38 -0600

    in the process of making fixes

--- MakeTrainingFiles.py
+++ MakeTrainingFiles.py
@@ -3,27 +3,29 @@ import os
 import json
 
 def makeTrainingFiles(path):
-    json_files = [a_json for a_json in os.listdir(output_file) if a_json.endswith('.json')]
+    json_files = [a_json for a_json in os.listdir(path) if a_json.endswith('.json')]
     for file in json_files:
-        with open(file, 'r') as fp:
+        with open(path + '/' + file, 'r') as fp:
             dictList = json.load(fp)
             for dict in dictList:
                 for sentence in dict['body']:
-                    print('Need to change to and from key recognition')
+                    # print('Need to change to and from key recognition')
 
                     if dict['from'] == 'Jeb Bush':
-                        filename = '/data/fromJeb.txt'
+                        filename = 'data/fromJeb.txt'
                     else:
-                        filename = '/data/toJeb.txt'
+                        filename = 'data/toJeb.txt'
 
-                    with open(filename, 'w') as file:
-                            file.write(sentence)
+                    with open(filename, 'a') as file:
+                        file.write(sentence)
+        break
 
 
 def main():
-    output_file = Path("/output/")
+    output_file = Path("output")
+    output_file_string = "output"
     if output_file.is_dir():
-        makeTrainingFiles(output_file)
+        makeTrainingFiles(output_file_string)
     else:
         print('No valid output folder available')
 
